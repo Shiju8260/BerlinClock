eece46c5069c6948cddc9eb67949d054a52fe773
Shiju <shijub8260@gmail.com>
2024-09-29 08:50:45 +0530

    Clockusecase injected into clock viewmodel to collect the the clock state on every second

--- app/src/main/java/com/berlinclock/kata/presentation/clock/ClockViewModel.kt
+++ app/src/main/java/com/berlinclock/kata/presentation/clock/ClockViewModel.kt
@@ -1,13 +1,20 @@
 package com.berlinclock.kata.presentation.clock
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.berlinclock.kata.domain.models.ClockState
+import com.berlinclock.kata.domain.usecase.ClockUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
 
 @HiltViewModel
-class ClockViewModel(): ViewModel() {
+class ClockViewModel @Inject constructor(
+    private val clockUseCase: ClockUseCase
+): ViewModel() {
 
     private val _state = MutableStateFlow(ClockState())
     val state: StateFlow<ClockState> = _state
@@ -17,6 +24,8 @@ class ClockViewModel(): ViewModel() {
     }
 
     private fun startClock() {
-        TODO("Not yet implemented")
+        clockUseCase.invoke().onEach { result ->
+            _state.value = result
+        }.launchIn(viewModelScope)
     }
 }
