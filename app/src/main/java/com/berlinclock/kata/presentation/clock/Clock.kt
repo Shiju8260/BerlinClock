2baeddb121a720e898c51e2718038c7f4a595a3a
Shiju <shijub8260@gmail.com>
2024-09-29 09:32:58 +0530

    Text view created for displaying normal time from clockstate

--- app/src/main/java/com/berlinclock/kata/presentation/clock/Clock.kt
+++ app/src/main/java/com/berlinclock/kata/presentation/clock/Clock.kt
@@ -1,10 +1,30 @@
 package com.berlinclock.kata.presentation.clock
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun Clock(clockViewModel: ClockViewModel = hiltViewModel(), modifier: Modifier) {
-
+fun Clock(viewModel: ClockViewModel = hiltViewModel(), modifier: Modifier) {
+    val state by viewModel.state.collectAsState()
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize().padding(16.dp)
+    ) {
+        Text(
+            text = state.time?:"",
+            fontSize = 24.sp
+        )
+    }
 }
