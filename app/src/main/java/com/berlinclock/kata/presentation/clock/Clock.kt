f5b191dc5110ae41ff46292388ff48ddbd470893
Shiju <shijub8260@gmail.com>
2024-09-29 09:27:41 +0530

    Hilt navigation dependency updated for instantiating viewmodel

--- app/src/main/java/com/berlinclock/kata/presentation/clock/Clock.kt
+++ app/src/main/java/com/berlinclock/kata/presentation/clock/Clock.kt
@@ -2,8 +2,9 @@ package com.berlinclock.kata.presentation.clock
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun Clock(modifier: Modifier) {
+fun Clock(clockViewModel: ClockViewModel = hiltViewModel(), modifier: Modifier) {
 
 }
