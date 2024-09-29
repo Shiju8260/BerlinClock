fb7ef3af0cf49fb5cd72492efe87465d20b8c482
Shiju <shijub8260@gmail.com>
2024-09-29 09:22:09 +0530

    Clock composable function created

---
+++ app/src/main/java/com/berlinclock/kata/presentation/clock/Clock.kt
@@ -0,0 +1,9 @@
+package com.berlinclock.kata.presentation.clock
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Clock(modifier: Modifier) {
+
+}
